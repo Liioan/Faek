dbfe5d78312d36bb1fb72054df2bf0fbb8c6f4a3
Dawid <dev.davidbijok@gmail.com>
2024-11-19 12:11:59 +0100

    add configuration

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -1,9 +1,20 @@
 package configuration
 
-import "os"
+import (
+	"encoding/json"
+	"errors"
+	"os"
+
+	e "github.com/liioan/faek/internal/errors"
+)
 
 const settingsFilePath = "/faek_settings.json"
 const settingsDirectoryPath = "/.config/faek"
+
+type Settings struct {
+	OutputStyle string `json:"outputStyle"`
+	FileName    string `json:"fileName"`
+}
 
 func getConfigDirectory() (string, error) {
 	dirname, err := os.UserHomeDir()
@@ -18,4 +29,65 @@ func getConfigFilePath() (string, error) {
 		return "", err
 	}
 	return dirname + settingsFilePath, nil
+}
+
+func SaveUserSettings(outputStyle, fileName string) error {
+	settings := Settings{OutputStyle: outputStyle, FileName: fileName}
+
+	bytes, err := json.Marshal(settings)
+	if err != nil {
+		return errors.New(e.CantMarshalJson)
+	}
+
+	filePath, err := getConfigDirectory()
+	if err != nil {
+		return errors.New(e.CantCreateConfigDirectory)
+	}
+
+	os.MkdirAll(filePath, 0755)
+	filePath, err = getConfigFilePath()
+	if err != nil {
+		return errors.New(e.CantCreateConfigDirectory)
+	}
+
+	file, _ := os.Create(filePath)
+	_, err = file.Write(bytes)
+
+	if err != nil {
+		return errors.New(e.CanSaveToFile)
+	}
+	return nil
+}
+
+func GetUserSettings() (Settings, error) {
+	defaultSettings := Settings{
+		OutputStyle: "terminal",
+		FileName:    "faekOutput.ts",
+	}
+
+	filePath, err := getConfigFilePath()
+	if err != nil {
+
+		return Settings{}, errors.New(e.FileDoesNotExists)
+	}
+	fileBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return Settings{}, errors.New(e.FileDoesNotExists)
+	}
+	s := Settings{}
+	err = json.Unmarshal(fileBytes, &s)
+
+	if err != nil {
+		return Settings{}, errors.New(e.CantUnmarshalJson)
+	}
+
+	if s.FileName == "" {
+		s.FileName = defaultSettings.FileName
+	}
+
+	if s.OutputStyle == "" {
+		s.OutputStyle = defaultSettings.OutputStyle
+	}
+
+	return s, nil
 }
